ecbe0dc9d78e9209268a3045751ea3a3af86a06d
vineefreitas <viniciusfreitasneri@hotmail.com>
2023-10-04 19:50:19 +0000

    feat(script.js): criação do primeiro programa

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
-console.log("Olá, mundo!");
+// lógica de programação
+// problema: obter 3 notas de um aluno , calcular a média,
+// exibir a média e exibir a situação do aluno: aprovado,
+// em exame ou reprovado.
 
-document.write("Olá, documento!");
+console.log('Inicio do programa');
 
-window.alert("Olá, Janela!")
+// declaração das variáveis
+
+const nota1 = parseInt(prompt('Digite a nota 1:'));
+const nota2 = parseInt(prompt('Digite a nota 2:'));
+const nota3 = parseInt(prompt('Digite a nota 3:'));
+
+// cáuculo da média
+media = (nota1 + nota2 + nota3) / 3;
+
+console.log ('A 1ª nota é:' , nota1)
+console.log ('A 2ª nota é:' , nota2)
+console.log ('A 3ª nota é:' , nota3)
+
+console.log ('A média do aluno é:' , media)
