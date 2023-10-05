c407d7c5e68d5fedd0b76a5048567607481cc9d1
vineefreitas <viniciusfreitasneri@hotmail.com>
2023-10-05 19:44:55 +0000

    feat(script.js): criação de interface interativa para cálculo de média de um aluno

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+function mediaAluno() {
+
 // lógica de programação
 // problema: obter 3 notas de um aluno , calcular a média,
 // exibir a média e exibir a situação do aluno: aprovado,
@@ -7,9 +9,9 @@ console.log('Inicio do programa');
 
 // declaração das variáveis
 
-const nota1 = parseInt(prompt('Digite a nota 1:'));
-const nota2 = parseInt(prompt('Digite a nota 2:'));
-const nota3 = parseInt(prompt('Digite a nota 3:'));
+const nota1 = parseInt(document.getElementById('nota1').value);
+const nota2 = parseInt(document.getElementById('nota2').value);
+const nota3 = parseInt(document.getElementById('nota3').value);
 
 // cáuculo da média
 media = (nota1 + nota2 + nota3) / 3;
@@ -18,4 +20,16 @@ console.log ('A 1ª nota é:' , nota1)
 console.log ('A 2ª nota é:' , nota2)
 console.log ('A 3ª nota é:' , nota3)
 
-console.log ('A média do aluno é:' , media)
+document.getElementById('resultado').innerHTML = 'A média do aluno é:' + media + '<br>' ;
+
+if (media >= 7) {
+    document.getElementById('resultado').innerHTML += 'Situação: Aprovado';
+} else if (media >= 5) {
+    document.getElementById('resultado').innerHTML += 'Situação: Exame';
+} else {
+    document.getElementById('resultado').innerHTML += 'Situação: Reprovado';
+}
+
+console.log ('Fim do programa.')
+
+}
